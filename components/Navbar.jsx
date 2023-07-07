400d93334fe787d61cac8780771c68e62e18a771
TomoFromEarth <mikedonothackmethanks@gmail.com>
2023-07-06 23:07:12 -0700

    created responsive navbar component

---
+++ components/Navbar.jsx
@@ -0,0 +1,133 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
+const Navbar = () => {
+  const isUserLoggedIn = true;
+  const [providers, setProviders] = useState(null);
+  const [toggleDropdown, settoggleDropdown] = useState(false);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    };
+    setProviders();
+  }, []);
+
+  return (
+    <nav className="flex-between mb-16 w-full pt-3">
+      <Link href="/" className="flex-center flex gap-2">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Authorization Practice</p>
+      </Link>
+
+      {/* Desktop Nav */}
+      <div className="hidden sm:flex">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-message" className="black_btn">
+              Leave Message
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/* Mobile Navigation */}
+      <div className="relative flex sm:hidden">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              src="/assets/images/logo.svg"
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt="profile"
+              onClick={() => settoggleDropdown((prev) => !prev)}
+            />
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => settoggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-message"
+                  className="dropdown_link"
+                  onClick={() => settoggleDropdown(false)}
+                >
+                  Leave Message
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    settoggleDropdown(false);
+                    signOut();
+                  }}
+                  className="black_btn mt-5 w-full"
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
