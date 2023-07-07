ec843017dd667cfeb7b8975e37e5c107ac688a52
TomoFromEarth <mikedonothackmethanks@gmail.com>
2023-07-06 18:24:42 -0700

    created initial home page look and added text

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,19 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="flex-center w-full flex-col">
+      <h1 className="head_text text-center">
+        This is a practice project to learn how to build a full stack app using
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> Next.js 13</span>
+      </h1>
+      <p className="desc text-center">
+        The idea behind this simple project is to use Next.js 13 and also the
+        NextAuth library to handle account creation and authorization.
+      </p>
+
+      {/* Feed */}
+    </section>
+  );
 };
 
 export default Home;
